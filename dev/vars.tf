fa200d10146a1f8c42668fd3bbe5b1735008dbc0
Ekudretli <erhankudretli@gmail.com>
2025-10-13 00:51:03 +0200

    feat:terraform-docs feature

--- dev/vars.tf
+++ dev/vars.tf
@@ -1,3 +1,13 @@
-variable "env" {}
-variable "owner" {}
-variable "project" {}
+variable "env" {
+  description = "The deployment environment (e.g., 'dev', 'prd'). Used for naming resources and tagging."
+  type        = string
+}
+variable "owner" {
+  description = "The name or ID of the team/individual responsible for this infrastructure."
+  type        = string
+}
+variable "project" {
+  description = "The name of the project this infrastructure belongs to (e.g., 'ikerian-challenge')."
+  type        = string
+}
+data "aws_caller_identity" "current" {} #to help terraform-docs to understand the Providers is using 
